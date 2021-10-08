33ee068f1cf2e380e442dc57678b4bc16e85562f
Sandy Ryza <sandy@elementl.com>
2021-10-08 16:31:29 -0700

    HN demo pipeline code improvements

--- examples/hacker_news/hacker_news/sensors/hn_tables_updated_sensor.py
+++ examples/hacker_news/hacker_news/sensors/hn_tables_updated_sensor.py
@@ -1,54 +1,59 @@
-from dagster import AssetKey, EventRecordsFilter, RunRequest, sensor
+from typing import Optional
 
-from ..jobs.story_recommender import story_recommender_prod_job, story_recommender_staging_job
-
-
-@sensor(job=story_recommender_prod_job)
-def story_recommender_on_hn_table_update_prod(context):
-    yield from _story_recommender_on_hn_table_update(context)
-
-
-@sensor(job=story_recommender_staging_job)
-def story_recommender_on_hn_table_update_staging(context):
-    yield from _story_recommender_on_hn_table_update(context)
+from dagster import (
+    AssetKey,
+    EventRecordsFilter,
+    JobDefinition,
+    RunRequest,
+    SensorDefinition,
+    sensor,
+)
 
 
-def _story_recommender_on_hn_table_update(context):
-    """Kick off a run if both the stories and comments tables have received updates"""
-    if context.last_run_key:
-        last_comments_key, last_stories_key = list(map(int, context.last_run_key.split("|")))
-    else:
-        last_comments_key, last_stories_key = None, None
+def make_hn_tables_updated_sensor(
+    job: Optional[JobDefinition] = None,
+    pipeline_name: Optional[str] = None,  # legacy arg
+    mode: Optional[str] = None,  # legacy arg
+) -> SensorDefinition:
+    """
+    Returns a sensor that launches the given job when the HN "comments" and "stories" tables have
+    both been updated.
+    """
 
-    comments_event_records = context.instance.get_event_records(
-        EventRecordsFilter(
-            asset_key=AssetKey(["snowflake", "hackernews", "comments"]),
-            after_cursor=last_comments_key,
-        ),
-        ascending=False,
-        limit=1,
+    @sensor(
+        name=f"{job.name}_on_hn_tables_updated", job=job, pipeline_name=pipeline_name, mode=mode
     )
+    def hn_tables_updated_sensor(context):
+        if context.last_run_key:
+            last_comments_key, last_stories_key = list(map(int, context.last_run_key.split("|")))
+        else:
+            last_comments_key, last_stories_key = None, None
 
-    stories_event_records = context.instance.get_event_records(
-        EventRecordsFilter(
-            asset_key=AssetKey(["snowflake", "hackernews", "stories"]),
-            after_cursor=last_stories_key,
-        ),
-        ascending=False,
-        limit=1,
-    )
+        comments_event_records = context.instance.get_event_records(
+            EventRecordsFilter(
+                asset_key=AssetKey(["snowflake", "hackernews", "comments"]),
+                after_cursor=last_comments_key,
+            ),
+            ascending=False,
+            limit=1,
+        )
 
-    if not comments_event_records or not stories_event_records:
-        return
+        stories_event_records = context.instance.get_event_records(
+            EventRecordsFilter(
+                asset_key=AssetKey(["snowflake", "hackernews", "stories"]),
+                after_cursor=last_stories_key,
+            ),
+            ascending=False,
+            limit=1,
+        )
 
-    last_comments_record_id = comments_event_records[0].storage_id
-    last_stories_record_id = stories_event_records[0].storage_id
-    run_key = f"{last_comments_record_id}|{last_stories_record_id}"
+        if not comments_event_records or not stories_event_records:
+            return
 
-    yield RunRequest(run_key=run_key)
+        last_comments_record_id = comments_event_records[0].storage_id
+        last_stories_record_id = stories_event_records[0].storage_id
+        run_key = f"{last_comments_record_id}|{last_stories_record_id}"
 
+        yield RunRequest(run_key=run_key)
 
-# legacy
-@sensor(pipeline_name="story_recommender", mode="prod")
-def story_recommender_on_hn_table_update(context):
-    yield from _story_recommender_on_hn_table_update(context)
+    return hn_tables_updated_sensor
